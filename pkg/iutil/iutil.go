004690449be33c5a5f9b32659c462d7f207d9462
Meghan Denny <nektromc@gmail.com>
2020-02-03 16:21:08 -0500

    add iutil.SplitLen

--- pkg/iutil/iutil.go
+++ pkg/iutil/iutil.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"io"
 	"io/ioutil"
+	"math"
 	"net/http"
 	"net/url"
 	"os"
@@ -133,4 +134,21 @@ func DownloadTo(urlS string, dir string, bar *mbpp.BarProxy) {
 	u, _ := url.Parse(urlS)
 	fn := u.Path[len(filepath.Dir(u.Path)):]
 	mbpp.CreateDownloadJob(urlS, dir+fn, bar)
+}
+
+// from SO
+func SplitLen(str string, size int) []string {
+	strLength := len(str)
+	splitedLength := int(math.Ceil(float64(strLength) / float64(size)))
+	splited := make([]string, splitedLength)
+	var start, stop int
+	for i := 0; i < splitedLength; i++ {
+		start = i * size
+		stop = start + size
+		if stop > strLength {
+			stop = strLength
+		}
+		splited[i] = str[start:stop]
+	}
+	return splited
 }
