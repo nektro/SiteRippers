69e4de848ab930e53a8126a70659f23f98a8d910
Meghan Denny <nektromc@gmail.com>
2020-02-03 17:43:54 -0500

    www.biodiversitylibrary.org - remove Log references

--- pkg/sites/www.biodiversitylibrary.org.go
+++ pkg/sites/www.biodiversitylibrary.org.go
@@ -1,7 +1,6 @@
 package sites
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
@@ -46,19 +45,16 @@ func init() {
 					iurlS := "https://www.biodiversitylibrary.org/item/" + n
 					res, err := http.Head(iurlS)
 					if err != nil {
-						fmt.Fprintln(idata.Log, 1, n, err)
 						return
 					}
 					if res.StatusCode != http.StatusOK {
 						if res.StatusCode == http.StatusNotFound {
 							return
 						}
-						fmt.Fprintln(idata.Log, 2, n, res.StatusCode)
 						return
 					}
 					doc, err := iutil.FetchDoc(iurlS, nil)
 					if err != nil {
-						fmt.Fprintln(idata.Log, 3, n, err)
 						return
 					}
 					t, _ := doc.Find(`meta[name="citation_title"]`).Attr("content")
